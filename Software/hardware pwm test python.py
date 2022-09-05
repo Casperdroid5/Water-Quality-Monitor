0925190c296252cd787ad1a352aeaa557e6a8604
Casperdroid5 <caspertak1@gmail.com>
2022-09-05 15:09:32 +0200

    added brightening part

--- Software/hardware pwm test python.py
+++ Software/hardware pwm test python.py
@@ -2,11 +2,21 @@ import pigpio
 import time
 
 pi = pigpio.pi()
+Highest_value = 1000000
+Lowest_value = 350000
+hardwareGPIOPin = 18
 
-pi.hardware_PWM(18, 0, 0) #Turn of PWM
+pi.hardware_PWM(hardwareGPIOPin, 0, 0) #Turn of PWM
 time.sleep (0.1)
-for dutycycle in range(1000000,360000, -100): #Slowly dimming the external led
-    pi.hardware_PWM(18, 100000, dutycycle) #Hardware pwm setting GPIO18 (pin 12), switching frequency (10000hz) and duty cycle 
+for dutycycle in range(Highest_value,Lowest_value, -100): #Slowly dimming the external led
+    pi.hardware_PWM(hardwareGPIOPin, 100000, dutycycle) #Hardware pwm setting GPIO18 (pin 12), switching frequency (10000hz) and duty cycle 
     #time.sleep (0)
     print(dutycycle)
-pi.hardware_PWM(18, 0, 0) #Turn of PWM
+pi.hardware_PWM(hardwareGPIOPin, 0, 0) #Turn of PWM
+
+time.sleep (0.1)
+for dutycycle in range(Lowest_value, Highest_value, 100): #Slowly brightening the external led
+    pi.hardware_PWM(hardwareGPIOPin, 100000, dutycycle) #Hardware pwm setting GPIO18 (pin 12), switching frequency (10000hz) and duty cycle 
+    #time.sleep (0)
+    print(dutycycle)
+pi.hardware_PWM(hardwareGPIOPin, 0, 0) #Turn of PWM
