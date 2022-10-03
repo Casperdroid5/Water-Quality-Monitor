b42ffef850001513c2574e40a909a7d90b3596d7
Casperdroid5 <caspertak1@gmail.com>
2022-10-03 15:46:44 +0200

    added new libarries and jlcpcb part plugin

--- Software/PyQt tryout/src/GUI.py
+++ Software/PyQt tryout/src/GUI.py
@@ -1,20 +1,35 @@
-from PyQt5.QtWidgets import (
-  QApplication,
-  QMainWindow,
-  QPushButton,
-)
 import sys
+# importing libraries
+from tokenize import Pointfloat
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
 class MainWindow(QMainWindow):
-  def __init__(self):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        self.setCentralWidget(self)  # Set the central widget of the Window.
+
+
+class Button(QMainWindow):
     super().__init__()
-    self.setWindowTitle("My App")
-    button = QPushButton("Press Me!")
-    button.setCheckable(True)
-    button.clicked.connect(self.the_button_was_clicked)
-    # Set the central widget of the Window.
-    self.setCentralWidget(button)
-  def the_button_was_clicked(self):
-    print("Clicked!")
+
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        self.setWindowTitle("My Oneshot App")  # Also change the window title.
+
+
+class Dial(QMainWindow):
+    def __init__(self):
+        super().MainWindow()
+        dial = QDial("Turn me!")
+        dial.rotated.connect(self.the_dial_was_rotated)
+
+
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
