e6dedc9796d7afc9342273cfca588efa0bb0172d
Casperdroid5 <caspertak1@gmail.com>
2022-11-14 12:35:33 +0100

    trying camera

--- Software/Camera/button.py
+++ Software/Camera/button.py
@@ -1,20 +1,27 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
+
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
-    self.setWindowTitle("My App")
-    self.button = QPushButton("Press Me!") 
-    self.button.clicked.connect(self.the_button_was_clicked)
-    # Set the central widget of the Window.
-    self.setCentralWidget(self.button)
-    def the_button_was_clicked(self):
-    self.button.setText("You already clicked me.") 
-    self.button.setEnabled(False) 
-    # Also change the window title.
-    self.setWindowTitle("My Oneshot App")
-    
+    # Create scene
+    self.image_item = QGraphicsPixmapItem()
+    scene = QGraphicsScene(self)
+    scene.addItem(self.image_item)
+
+    # Create GraphicView display
+    self.view = QGraphicsView(scene, self)
+    # Adding right click menus
+    self.view.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
+    self.zoomout_action = QAction("Fit canvas", self)
+    self.view.addAction(self.zoomout_action)
+
+
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec_()
+
+image = QImage(camera_image, w, h, w, QImage.Format_Grayscale8)
+self.image_item.setPixmap(QPixmap.fromImage(image))
+self.view.fitInView(self.image_item)
