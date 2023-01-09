621818bd1d4d22a5d42209ebd88f6cb46d0bf7a3
Casperdroid5 <caspertak1@gmail.com>
2023-01-09 13:34:33 +0100

    added intro text to i2c testcode

--- Software/RPI i2c test using adt7410/testi2cusingadt7410.py
+++ Software/RPI i2c test using adt7410/testi2cusingadt7410.py
@@ -2,6 +2,12 @@ import time
 import board
 import adafruit_adt7410
 
+
+"""
+This code is primarely used to test if an i2c device is connected properly to the raspberry pi.
+In this case we are using an temperature sensor.
+"""
+
 i2c = board.I2C()  # uses board.SCL and board.SDA
 adt = adafruit_adt7410.ADT7410(i2c, address=0x48)
 adt.high_resolution = True
