479f0e7924bba413418112aa3ff714d307bc4d5c
Casperdroid5 <caspertak1@gmail.com>
2023-01-09 11:32:27 +0100

    Finished UARTsteppermotor control software (not tested)

--- Software/Rastaban HAT Functional code/UARTsteppermotor.py
+++ Software/Rastaban HAT Functional code/UARTsteppermotor.py
@@ -18,37 +18,38 @@ class UARTSTEPPERMOTORS():
         self.state = None
 
     def ControlMotorMovement(self, dirreg: bool, rpm: int, revolutions: float, State: bool):    
-        tmc.set_direction_reg(dirreg)
-        set_vactual_rpm(rpm, revolutions = revolutions)
-        tmc.set_motor_enabled(State)
+        self.set_direction_reg(dirreg)
+        self.set_vactual_rpm(rpm, revolutions = revolutions)
+        self.set_motor_enabled(State)
         
     def SetMotorSettings(self, cur: int, interpol: bool, spread: bool, microstepres: int, interrsense: bool): 
-        tmc.set_current(cur)
-        tmc.set_interpolation(interpol)
-        tmc.set_spreadcycle(spread)
-        tmc.set_microstepping_resolution(microstepres)
-        tmc.set_internal_rsense(interrsense)
+        self.set_current(cur)
+        self.set_interpolation(interpol)
+        self.set_spreadcycle(spread)
+        self.set_microstepping_resolution(microstepres)
+        self.set_internal_rsense(interrsense)
     
     def PrintCurrentSettings(self): 
-        tmc.readIOIN()
-        tmc.readCHOPCONF()
-        tmc.readDRVSTATUS()
-        tmc.readGCONF()
+        self.readIOIN()
+        self.readCHOPCONF()
+        self.readDRVSTATUS()
+        self.readGCONF()
         print("---\n---") # for readability
 
 
 if __name__ == "__main__":
-   tmc = UARTSTEPPERMOTORS(21)
-   #tmc.set_loglevel(Loglevel.DEBUG)
-   #tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
-   tmc.set_motor_enabled(True)
+   primarystepper = UARTSTEPPERMOTORS(21) 
+   primarystepper = primarystepper.SetMotorSettings(900, True, False, 256, False)
+   primarystepper = primarystepper.PrintCurrentSettings()
+   #primarystepper.set_loglevel(Loglevel.DEBUG)
+   #primarystepper.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+   primarystepper = primarystepper.ControlMotorMovement(False, )
+  
+   primarystepper.set_motor_enabled(True)
 
 
-#-----------------------------------------------------------------------
-# deinitiate the TMC_2209 class
-#-----------------------------------------------------------------------
-tmc.deinit()
-del tmc
+#primarystepper.deinit()
+#del primarystepper
 
 print("---")
 print("SCRIPT FINISHED")
