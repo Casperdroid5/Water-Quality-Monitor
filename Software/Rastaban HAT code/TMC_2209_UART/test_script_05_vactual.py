edac444d8570df54575e45beb2a6cf0177335c5c
Casperdroid5 <caspertak1@gmail.com>
2023-01-19 10:58:28 +0100

    Fixed the stepper motor issues with UART
    -documentation on fix added
    -updated scripts to work with uart
    -added uart logic analyser data for documentation

--- Software/Rastaban HAT code/TMC_2209_UART/test_script_05_vactual.py
+++ Software/Rastaban HAT code/TMC_2209_UART/test_script_05_vactual.py
@@ -15,10 +15,9 @@ print("---")
 # initiate the TMC_2209 class
 # use your pin for pin_en here
 #-----------------------------------------------------------------------
-tmc = TMC_2209(21)
-
-
-
+tmc0 = TMC_2209(27, driver_address=0)
+tmc1 = TMC_2209(22, driver_address=1)
+tmc2 = TMC_2209(19, driver_address=2)
 
 
 #-----------------------------------------------------------------------
@@ -26,98 +25,26 @@ tmc = TMC_2209(21)
 # set whether the movement should be relative or absolute
 # both optional
 #-----------------------------------------------------------------------
-tmc.set_loglevel(Loglevel.DEBUG)
-tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
-
-
-
-
+tmc0.set_loglevel(Loglevel.DEBUG)
+tmc0.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
 
 #-----------------------------------------------------------------------
 # these functions change settings in the TMC register
 #-----------------------------------------------------------------------
-tmc.set_direction_reg(False)
-tmc.set_current(300)
-tmc.set_interpolation(True)
-tmc.set_spreadcycle(False)
-tmc.set_microstepping_resolution(2)
-tmc.set_internal_rsense(False)
+tmc0.set_direction_reg(False)
+tmc0.set_current(300)
+tmc0.set_interpolation(True)
+tmc0.set_spreadcycle(False)
+tmc0.set_microstepping_resolution(2)
+tmc0.set_internal_rsense(False)
 
 
 print("---\n---")
-
-
-
-
-
-#-----------------------------------------------------------------------
-# these functions read and print the current settings in the TMC register
-#-----------------------------------------------------------------------
-tmc.readIOIN()
-tmc.readCHOPCONF()
-tmc.readDRVSTATUS()
-tmc.readGCONF()
-
-print("---\n---")
-
-
-
-
 
 #-----------------------------------------------------------------------
 # activate the motor current output
 #-----------------------------------------------------------------------
-tmc.set_motor_enabled(True)
-
-
-
-
-
-#-----------------------------------------------------------------------
-# move the motor for 1 second forward, stop for 1 second
-# and then move backwards for 1 second
-#-----------------------------------------------------------------------
-#tmc.set_vactual(400)
-#time.sleep(1)
-#tmc.set_vactual(0)
-#time.sleep(1)
-#tmc.set_vactual(-400)
-#time.sleep(1)
-#tmc.set_vactual(0)
-
-
-
-
-
-#-----------------------------------------------------------------------
-# set_vactual_rps uses revolutions per seconds as parameter
-#-----------------------------------------------------------------------
-# tmc.set_vactual_rps(1)
-# time.sleep(1)
-# tmc.set_vactual_rps(0)
-# time.sleep(1)
-# tmc.set_vactual_rps(-1)
-# time.sleep(1)
-# tmc.set_vactual_rps(0)
-
-
-
-
-
-#-----------------------------------------------------------------------
-# set_vactual_rps uses revolutions per seconds as parameter
-#-----------------------------------------------------------------------
-#tmc.set_vactual_rpm(60)
-#time.sleep(1)
-#tmc.set_vactual(0)
-#time.sleep(1)
-#tmc.set_vactual_rpm(-60)
-#time.sleep(1)
-#tmc.set_vactual(0)
-
-
-
-
+tmc0.set_motor_enabled(True)
 
 #-----------------------------------------------------------------------
 # set_vactual_rpm and set_vactual_rps accept "revolutions" and "duration" as keyword parameter
@@ -125,30 +52,17 @@ tmc.set_motor_enabled(True)
 # if revolutions the script will calculate the duration based on the speed and the revolutions
 # Movement of the Motor will not be very accurate with this way
 #-----------------------------------------------------------------------
-tmc.set_vactual_rpm(30, revolutions=2)
-tmc.set_vactual_rpm(-120, revolutions=2)
+tmc0.set_vactual_rpm(30, revolutions=2)
+tmc0.set_vactual_rpm(-120, revolutions=2)
 time.sleep(1)
-tmc.set_vactual_rpm(30, duration=4)
-tmc.set_vactual_rpm(-120, duration=1)
-
-
-
-
-
-#-----------------------------------------------------------------------
-# use acceleration (velocity ramping) with VActual
-# does not work with revolutions as parameter
-#-----------------------------------------------------------------------
-# tmc.set_vactual_rpm(-120, duration=10, acceleration=500)
-
-
-
+tmc0.set_vactual_rpm(30, duration=4)
+tmc0.set_vactual_rpm(-120, duration=1)
 
 
 #-----------------------------------------------------------------------
 # deactivate the motor current output
 #-----------------------------------------------------------------------
-tmc.set_motor_enabled(False)
+tmc0.set_motor_enabled(False)
 
 print("---\n---")
 
@@ -159,8 +73,8 @@ print("---\n---")
 #-----------------------------------------------------------------------
 # deinitiate the TMC_2209 class
 #-----------------------------------------------------------------------
-tmc.deinit()
-del tmc
+tmc0.deinit()
+del tmc0
 
 print("---")
 print("SCRIPT FINISHED")
--- Software/Rastaban HAT code/TMC_2209_UART/test_script_06_multiple_drivers.py
+++ Software/Rastaban HAT code/TMC_2209_UART/test_script_06_multiple_drivers.py
@@ -13,11 +13,11 @@ print("---")
 
 #-----------------------------------------------------------------------
 # initiate the TMC_2209 class
-# use your pins for pin_en, pin_step, pin_dir here
+# use your pins for pin_en, pin_step, pin_dir here (step and dir can be left out)
 #-----------------------------------------------------------------------
-tmc1 = TMC_2209(21, 16, 20, driver_address=0)
-tmc2 = TMC_2209(26, 13, 19, driver_address=1)
-
+tmc0 = TMC_2209(27, driver_address=0)
+tmc1 = TMC_2209(22, driver_address=1)
+tmc2 = TMC_2209(19, driver_address=2)
 
 
 
@@ -27,6 +27,9 @@ tmc2 = TMC_2209(26, 13, 19, driver_address=1)
 # set whether the movement should be relative or absolute
 # both optional
 #-----------------------------------------------------------------------
+tmc0.set_loglevel(Loglevel.DEBUG)
+tmc0.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+
 tmc1.set_loglevel(Loglevel.DEBUG)
 tmc1.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
 
@@ -34,10 +37,17 @@ tmc2.set_loglevel(Loglevel.DEBUG)
 tmc2.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
 
 
-
 #-----------------------------------------------------------------------
 # these functions read and print the current settings in the TMC register
 #-----------------------------------------------------------------------
+
+print("---")
+print("IOIN tmc0")
+print("---")
+tmc0.readIOIN()
+
+print("---\n---")
+
 
 print("---")
 print("IOIN tmc1")
@@ -56,15 +66,16 @@ print("---\n---")
 
 
 
-
-
 #-----------------------------------------------------------------------
 # deinitiate the TMC_2209 class
 #-----------------------------------------------------------------------
+tmc0.set_motor_enabled(False)
 tmc1.set_motor_enabled(False)
 tmc2.set_motor_enabled(False)
+tmc0.deinit()
 tmc1.deinit()
 tmc2.deinit()
+del tmc0
 del tmc1
 del tmc2
 
